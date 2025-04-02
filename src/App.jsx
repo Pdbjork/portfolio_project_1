f61b938b637732634476697e331b5cc78e0918ab
Pdbjork <pdbjork@gmail.com>
2025-04-02 15:53:53 -0500

    Update App.jsx
    add contact info

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Peter Bjork",
+  title: "Developer & AI Engineer",
+  email: "pdbjork@gmail.com",
+  gitHub: "pdbjork",
+  instagram: "pedro_bjork",
+  linkedIn: "peter-bjork",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "porkbjork",
+  youTube: "pdbjork",
 };
 
 const primaryColor = "#4E567E";
